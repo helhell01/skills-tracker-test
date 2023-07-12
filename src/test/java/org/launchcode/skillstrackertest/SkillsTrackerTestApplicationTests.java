04c85b6ce1644435aa17c4aaf5af32dfe1805042
Hanna Luechtefeld <hannaluechtefeld@gmail.com>
2023-07-12 17:41:58 -0500

    Initial commit

---
+++ src/test/java/org/launchcode/skillstrackertest/SkillsTrackerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.skillstrackertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillsTrackerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
